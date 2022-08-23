c32ab199dd30e6fe2d89f84c975aca97a02a308d
sardorziyodullayev <sardorziyodullayev2004@gmail.com>
2022-08-23 16:51:26 +0500

    Initialize commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let elSiteDiv = document.querySelector(".content");
+
+pokemons.forEach(function (pokemon) {
+	let elListDiv = document.createElement("div");
+	let elListImage = document.createElement("img");
+	let elListTitle = document.createElement("h3");
+	let elListText = document.createElement("p");
+
+	elListDiv.className = "col-4 w-25 card m-2 ms-auto";
+	elListDiv.setAttribute("style", "width: 18rem;");
+	elListImage.setAttribute("class", "card-img-top");
+
+	elListImage.src = pokemon.img;
+	elListTitle.textContent = pokemon.name;
+	elListText.textContent = pokemon.weaknesses;
+
+	elListDiv.append(elListImage, elListTitle, elListText);
+	elSiteDiv.appendChild(elListDiv);
+});
